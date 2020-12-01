7ed50e0cd6048a78c9d7e7cc1e10fbc22f39191c
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-11-30 23:05:40 -0800

    add navbar.js script for season/postseason dropdowns

---
+++ static/js/navbar.js
@@ -0,0 +1,62 @@
+(function () {
+
+  var baseApiUrl = 'http://192.168.30.20:8989';
+  var baseUIUrl  = 'http://192.168.30.20:8001';
+
+  var Navbar = {
+
+    baseApiUrl : baseApiUrl,
+    baseUIUrl : baseUIUrl,
+
+    init : function() {
+      // get current day/season info from API /today
+      var seasonsUrl = this.baseApiUrl + '/seasons';
+      fetch(seasonsUrl)
+      .then(res => res.json())
+      .then((seasonsApiResult) => {
+
+        var navbarSeasonDropdown = document.getElementById('navbar-season-dropdown-menu');
+        var navbarPostseasonDropdown = document.getElementById('navbar-postseason-dropdown-menu');
+
+        for (var season0 in seasonsApiResult) {
+          var sp1 = parseInt(season0) + 1;
+
+          var sdropElem = document.createElement('a');
+          sdropElem.classList.add('dropdown-item');
+          sdropElem.setAttribute('href', baseUIUrl + '/season.html?season=' + season0);
+          sdropElem.innerHTML = 'Season ' + sp1;
+          navbarSeasonDropdown.appendChild(sdropElem);
+
+          var pdropElem = document.createElement('a');
+          pdropElem.classList.add('dropdown-item');
+          pdropElem.setAttribute('href', baseUIUrl + '/postseason.html?season=' + season0);
+          pdropElem.innerHTML = 'Season ' + sp1;
+          navbarPostseasonDropdown.appendChild(pdropElem);
+
+        }
+
+      })
+      .catch(err => {
+        console.log(err);
+        //this.error(-1);
+      }); // end /seeds api call
+    },
+
+    /**
+     * Register Event
+     */
+    registerEvent : function (element, event, handler, capture) {
+      if (/msie/i.test(navigator.userAgent)) {
+        element.attachEvent('on' + event, handler);
+      } else {
+        element.addEventListener(event, handler, capture);
+      }
+    },
+
+  };
+
+  Navbar.registerEvent(window, 'load', function () {
+    Navbar.init();
+  }, false);
+
+}());
