8bab9b8d9b4be2a84ac1eebda583e58ff7eb2d50
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2020-12-08 14:03:52 -0800

    add prodcheck message

---
+++ static/js/prodcheck.js
@@ -0,0 +1,31 @@
+(function () {
+
+  var ProdCheck = {
+    baseApiUrl : getBaseApiUrl(),
+    baseUIUrl : getBaseUIUrl(),
+
+    init : function() {
+      if (baseUIUrl != "https://golly.life") {
+        var prodCheck = document.getElementById('prodcheck-msg');
+        prodCheck.classList.remove('invisible');
+      }
+    },
+
+    /**
+     * Register Event
+     */
+    registerEvent : function (element, event, handler, capture) {
+      if (/msie/i.test(navigator.userAgent)) {
+        element.attachEvent('on' + event, handler);
+      } else {
+        element.addEventListener(event, handler, capture);
+      }
+    }
+
+  };
+
+  ProdCheck.registerEvent(window, 'load', function () {
+    ProdCheck.init();
+  }, false);
+
+}());
